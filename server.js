3b4ad85d1f7cb080add1090d911dae4d7ed1db59
himasnhusharma <himanshu.sharma@oodlestechnologies.com>
2017-02-07 19:23:36 +0530

    ch

--- server.js
+++ server.js
@@ -5,24 +5,24 @@ var path = require('path');
 var fs = require('fs');
 // var bodyParser = require('body-parser');
 
-var multer  = require('multer');
+var multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 var type = upload.single('file');
 
 var webshot = require('webshot');
 
 var webshotOptions = {
-  // screenSize: {
-  //   width: 768
-  // , height: 510
-  // },
-	shotSize: {
-    width: 610
-  , height: 500
-  }
+    // screenSize: {
+    //   width: 768
+    // , height: 510
+    // },
+    shotSize: {
+        width: 610,
+        height: 500
+    }
 };
 
- 
+
 
 
 // app.use(bodyParser({uploadDir:'/home/amit/Downloads'}));
@@ -36,15 +36,15 @@ app.use(express.static(path.join(__dirname, '/public/')));
 
 // viewed at http://localhost:8080
 app.get('/query', function(req, res) {
-	var age = req.query.age;
-	var cses = req.query.cses;
-	var thp = req.query.thp;
-	var fy = req.query.fy;
-    res.render(path.join(__dirname + '/views/index.ejs'),{
-    	age : age,
-    	cses : cses,
-    	thp : thp,
-    	fy : fy
+    var age = req.query.age;
+    var cses = req.query.cses;
+    var thp = req.query.thp;
+    var fy = req.query.fy;
+    res.render(path.join(__dirname + '/views/index.ejs'), {
+        age: age,
+        cses: cses,
+        thp: thp,
+        fy: fy
     });
     // res.sendFile(path.join(__dirname + '/views/index.html'));
     // res.redirect('/image');
@@ -52,80 +52,80 @@ app.get('/query', function(req, res) {
 
 
 app.get('/', function(req, res) {
-	
+
     res.sendFile(path.join(__dirname + '/views/index1.html'));
     // res.redirect('/image');
 });
 
-	app.get('/webshot', function(req, res,callback){
+app.get('/webshot', function(req, res, callback) {
     var ageWs = req.query.age;
-	var csesWs = req.query.cses;
-	var thpWs = req.query.thp;
-	var fyWs = req.query.fy;
+    var csesWs = req.query.cses;
+    var thpWs = req.query.thp;
+    var fyWs = req.query.fy;
 
-	var queryStringWs = "query?fy=" + fyWs + "&age=" + ageWs + "&cses=" + csesWs +"&thp=" + thpWs; 	
-	
-	var timeS= new Date;
-	var name= timeS.getTime() + ".png";
+    var queryStringWs = "query?fy=" + fyWs + "&age=" + ageWs + "&cses=" + csesWs + "&thp=" + thpWs;
 
-	// var end = Date.now() + 10000;
-	// console.log(end);
-	// while (Date.now() < end);
+    var timeS = new Date;
+    var name = timeS.getTime() + ".png";
 
-	function f1(){
-		webshot('http://180.151.85.194:3000/' + queryStringWs,'uploads/' + name, webshotOptions,function(err,data){
-    	// res.write("error saving");
+    // var end = Date.now() + 10000;
+    // console.log(end);
+    // while (Date.now() < end);
 
-      if(err){
-        console.log("error is",err);
-        var resErr = new Error("Unable to generate SSO chart");
-        resErr.status = 400;
-        console.log("error occured",resErr);
-          callback(resErr);
-      }else{
-        var img = fs.readFileSync('uploads/' + name);
-    console.log('uploads/' + name);
-  fs.unlink('uploads/' + name);
-    res.writeHead(200, {'Content-Type': 'image/png'  });
-    res.end(img, 'binary');
-      }
+    function f1() {
+        webshot('http://180.151.85.194:3000/' + queryStringWs, '../../calculators/incomeTaxCalculator/uploads/' + name, webshotOptions, function(err, data) {
+            // res.write("error saving");
+
+            if (err) {
+                console.log("error is", err);
+                var resErr = new Error("Unable to generate SSO chart");
+                resErr.status = 400;
+                console.log("error occured", resErr);
+                callback(resErr);
+            } else {
+                var img = fs.readFileSync('../../calculators/incomeTaxCalculator/uploads/' + name);
+                console.log('uploads/' + name);
+                fs.unlink('uploads/' + name);
+                res.writeHead(200, { 'Content-Type': 'image/png' });
+                res.end(img, 'binary');
+            }
 
 
-    	});
-	}
+        });
+    }
 
-	f1();
+    f1();
 
-	// function waitUntilFileExists(path, callback) {
- //  	fs.exists(path, function(exists) {
- //    if (!exists) {
- //      waitUntilFileExists(path, callback);
- //    } else {
- //      setTimeout(callback,4000);
- //    } 
- //  	});
-	// }
-    
- //    waitUntilFileExists('uploads/' + name,function(){
-	// var img = fs.readFileSync('uploads/' + name);
- //    console.log('uploads/' + name);
-	// fs.unlink('uploads/' + name);
- //    res.writeHead(200, {'Content-Type': 'image/png'	 });
- //    res.end(img, 'binary');
-	// }); 
+    // function waitUntilFileExists(path, callback) {
+    //    fs.exists(path, function(exists) {
+    //    if (!exists) {
+    //      waitUntilFileExists(path, callback);
+    //    } else {
+    //      setTimeout(callback,4000);
+    //    } 
+    //    });
+    // }
 
-	
+    //    waitUntilFileExists('uploads/' + name,function(){
+    // var img = fs.readFileSync('uploads/' + name);
+    //    console.log('uploads/' + name);
+    // fs.unlink('uploads/' + name);
+    //    res.writeHead(200, {'Content-Type': 'image/png'  });
+    //    res.end(img, 'binary');
+    // }); 
+
+
 
 });
 
-    app.get('/getImage', function(req, res) {
-    	var name= req.query.name;
-    	var img = fs.readFileSync('uploads/' + name);
+app.get('/getImage', function(req, res) {
+    var name = req.query.name;
+    var img = fs.readFileSync('uploads/' + name);
     console.log('uploads/' + name);
-	fs.unlink('uploads/' + name);
-    res.writeHead(200, {'Content-Type': 'image/png'	 });
+    fs.unlink('uploads/' + name);
+    res.writeHead(200, { 'Content-Type': 'image/png' });
     res.end(img, 'binary');
-    });
+});
 
 // module.exports = router;
 
